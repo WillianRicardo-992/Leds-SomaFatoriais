e70a48bd2863083f28493c5c792787b265c577cb
Willian Ricardo <willianricardoleal@gmail.com>
2025-04-18 22:14:27 -0300

    resolucao

---
+++ LED/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        // Dicionário para armazenar a quantidade de LEDs por dígito
+        Dictionary<char, int> ledsPorDigito = new Dictionary<char, int>()
+        {
+            {'0', 6},
+            {'1', 2},
+            {'2', 5},
+            {'3', 5},
+            {'4', 4},
+            {'5', 5},
+            {'6', 6},
+            {'7', 3},
+            {'8', 7},
+            {'9', 6}
+        };
+
+        // Leitura da quantidade de casos de teste
+        int N = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < N; i++)
+        {
+            string valor = Console.ReadLine();
+            int totalLeds = 0;
+
+            // Soma os LEDs necessários para cada dígito do número
+            foreach (char digito in valor)
+            {
+                totalLeds += ledsPorDigito[digito];
+            }
+
+            Console.WriteLine($"{totalLeds} leds");
+        }
+    }
+}
---
+++ SomaFatoriais/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string linha;
+        while ((linha = Console.ReadLine()) != null)
+        {
+            string[] partes = linha.Split(' ');
+            int M = int.Parse(partes[0]);
+            int N = int.Parse(partes[1]);
+
+            ulong fatorialM = Fatorial(M);
+            ulong fatorialN = Fatorial(N);
+
+            Console.WriteLine(fatorialM + fatorialN);
+        }
+    }
+
+    static ulong Fatorial(int n)
+    {
+        ulong resultado = 1;
+        for (int i = 2; i <= n; i++)
+        {
+            resultado *= (ulong)i;
+        }
+        return resultado;
+    }
+}
